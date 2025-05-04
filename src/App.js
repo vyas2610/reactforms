b15b9945b5d0fd083c555e2e3d21fbec5176a886
vyas2610 <deepakvyas261092@gmail.com>
2025-05-04 14:52:49 +0530

    Form Submit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("Deepak");
+  const [email, setEmail] = useState("deepak@gmail.com");
+  const changeHandler = (e) => {
+    if (e.target.name === "name") {
+      setName(e.target.value);
+    } else if (e.target.name === "email") {
+      setEmail(e.target.value);
+    }
+  };
+  const SudmitData = (e) => {
+    e.preventDefault();
+    let data = {
+      name,
+      email,
+    };
+    console.log(data);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container my-5">
+        <div className="row">
+          <div className="col">
+            <h1>Form Handling in React Js</h1>
+            <form action="" onSubmit={SudmitData}>
+              <p>
+                <input
+                  type="text"
+                  value={name}
+                  className="form-control"
+                  name="name"
+                  onChange={changeHandler}
+                />
+              </p>
+              <p>
+                <input
+                  type="email"
+                  name="email"
+                  value={email}
+                  className="form-control"
+                  onChange={changeHandler}
+                />
+              </p>
+              <p>
+                <input
+                  type="submit"
+                  value="Click"
+                  className=" btn btn-primary px-4"
+                ></input>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
